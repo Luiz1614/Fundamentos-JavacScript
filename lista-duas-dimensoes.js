e26e59f1df726a0f118ccd480f6cdf64fdac3a5b
Luiz Otávio <luizotavio.ok@gmail.com>
2023-02-21 16:44:52 -0300

    Jundando duas listas em uma

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ['João', 'Ana', 'Caio'];
+const media = [10, 8, 9];
+
+const listaGeral = [alunos, media] // Cria uma lista com duas outras listas dentro
+
+console.log('A aluna da posição 1 da lista de alunos é : ', listaGeral[0][2]); //Como acessar o elemento das listas alunos
+console.log('A média do aluno é : ', listaGeral[1][2]); //Como acessar o elemento da lista média
