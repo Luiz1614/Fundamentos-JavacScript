43e5a4f0ae08f1d454782db6f00d3f2df4b8e01c
Luiz Otávio <luizotavio.ok@gmail.com>
2023-02-21 15:13:13 -0300

    Aprendendo arrays

---
+++ adicionando-notas.js
@@ -0,0 +1,7 @@
+const nota = [10, 6, 8]
+
+nota.push(9) // inserindo um valor no array
+
+const media = (nota[0] + nota[1] + nota[2] + nota[3]) / nota.length
+
+console.log(media)
---
+++ media.js
@@ -0,0 +1,16 @@
+//---------utilizando arrays-------------
+
+
+//const nota1 = 10;
+//const nota2 = 6.5;
+//const nota3 = 8;
+//const nota4 = 7.5;
+
+
+//posições ou índices
+//              0   1   2   3
+const notas = [10, 6.5, 7, 7.5] //elementos / itens / valores
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
