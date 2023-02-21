d1d52240b958127064afc723e94fd95699d96f39
Luiz Otávio <luizotavio.ok@gmail.com>
2023-02-21 16:12:21 -0300

    Atualizando uma array com SPLICE

---
+++ atualizando.js
@@ -0,0 +1,5 @@
+const nomes = ['João', 'Ana', 'Caio', 'Mara', 'Marjorie', 'Léo']
+
+nomes.splice(1,2, 'Rodrigo') // Remove itens do meio da lista, sendo o primeiro indice o primeiro a ser removido, o segundo a quantidade de itens que desejamos remover e o terceiro o indice a entrar na lista 
+
+console.log(nomes)
