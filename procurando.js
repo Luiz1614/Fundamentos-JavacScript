e689b53152782c44db02748ab56bee0521753f75
Luiz Otávio <122511773+Luiz1614@users.noreply.github.com>
2023-02-27 12:18:27 -0300

    Pesquisando  alunos e médias dentro da tabela

--- procurando.js
+++ procurando.js
@@ -3,3 +3,24 @@ const media = [10, 8, 9];
 
 const listaGeral = [alunos, media] // Cria uma lista com duas outras listas dentro
 
+function exibeNomeENota(Aluno) {
+    if(listaGeral[0].includes(Aluno)){ // Includes confere se o que foi passado no parametro ta incluso na lista
+
+        //const alunos = listaGeral[0];
+        //const media = listaGeral[1];
+
+        const [alunos, media] = listaGeral // mesma função das declaraçoes de const acima
+
+        const indice = alunos.indexOf(Aluno) // IndexOf utilizado para mostrar o indice de algo
+
+        const MediaAluno = media [indice] // Pesquisa a lista de médias e mostra a nota do aluno
+
+        console.log(`A média de ${Aluno} é ${MediaAluno}`)
+
+    }
+    else{
+        console.log('Aluno não encontrado!')
+    }
+}
+
+exibeNomeENota('Ana')
